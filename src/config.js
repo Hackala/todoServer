2ecf3cbecd5c164b40383b08d2155cf07af12c84
paragon-edu <subscriptions@paragon.ba>
2023-04-09 15:01:32 +0200

    railway.app mongoDb added.

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ export default {
     env: 'development',
     port: 8080,
     secret: 'Time$ch00l',
-    mongo: 'mongodb+srv://sule:Abrakadabra4$@education.sqsev.mongodb.net/testera'
+    // mongo: 'mongodb+srv://sule:Abrakadabra4$@education.sqsev.mongodb.net/testera',
+    mongo: "mongodb://mongo:LCUxxJORI8Gc1R3hXBml@containers-us-west-63.railway.app:7488"
 }
