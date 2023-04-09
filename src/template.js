f58df95d702c22eb51a0db42e976b2219e86c3e5
paragon-edu <subscriptions@paragon.ba>
2023-04-09 14:51:28 +0200

    template updated.

--- src/template.js
+++ src/template.js
@@ -9,13 +9,6 @@ export default (title = 'TimeKeeper') => {
         </head>
         <body>
             <h2>Hello from ${title}</h2>
-            <p>Your user is<br>
-            ${config.currentUser.id}<br>
-            ${config.currentUser.name}<br>
-            ${config.currentUser.sub}<br>
-            ${config.currentUser.cli}<br>
-            ${config.currentUser.scopes[0].role}:${config.currentUser.scopes[0].action} <br>
-            ${config.token}
             <div>
             <a href='/auth/login'><button>Login</button></a>
             &nbsp;&nbsp;
