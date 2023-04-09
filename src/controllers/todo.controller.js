0f005efe962e03539c7834f34f84b71878634f34
paragon-edu <subscriptions@paragon.ba>
2023-04-09 14:32:47 +0200

    CRUD operations added.

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -8,18 +8,57 @@ const create = async (req, res, next) => {
     return res.status(200).json({ message: "Data successfully saved." })
   } catch (err) {
     console.log(err)
-    return res.status(400).json({ message: "Error saving data!" })
+    return res.status(400).json({ message: err.message })
   }
 }
 
-const list = (req, res) => { }
+const list = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos)
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json({ message: err.message })
+  }
+}
 
-const getId = (req, res, next, id) => { }
+const getId = async (req, res, next, id) => {
+  try {
+    let todo = await User.findById(id)
+    if (!todo) return res.status(404).json("Task not found!")
+    req.profile = todo
+    next()
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json({ message: err.message })
+  }
+}
 
-const read = (req, res) => { }
+const read = (req, res) => {
+  return res.json(req.profile)
+}
 
-const update = (req, res, next) => { }
+const update = async (req, res, next) => {
+  try {
+    let todo = req.profile
+    todo = _.extend(todo, req.body)
+    await todo.save()
+    read.status(200).json(user)
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json({ message: err.message })
+  }
+}
 
-const remove = (req, res, next) => { }
+const remove = async (req, res, next) => {
+  try {
+    let todo = req.profile
+    let deletedTask = await todo.remove()
+    res.status(200).json(deletedTask)
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json({ message: err.message })
+  }
+}
 
 export default { create, list, getId, read, update, remove }
